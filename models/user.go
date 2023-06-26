53472f0e7df225b95f0af6d9b732c0c0f65fca28
vasily.smolin <vasilysmoolin@gmail.com>
2023-06-26 17:30:31 +0400

    hidden password and relations

--- models/user.go
+++ models/user.go
@@ -12,9 +12,9 @@ type User struct {
 	PhoneVerify time.Time `json:"deleted_at,omitempty"`
 	Name string `gorm:"column:name;size:256;default:null;" json:"name" validate:"min=1,max=32"`
 	Surname string `gorm:"column:surname;size:256;default:null;" json:"surname" validate:"min=1,max=32"`
-	Password string `gorm:"column:password;size:256;default:null;" json:"password" validate:"min=1,max=32"`
+	Password string `gorm:"column:password;size:256;default:null;" json:"-" validate:"min=1,max=32"`
 	Avatar string `gorm:"column:avatar;size:3000;default:null;" json:"avatar" validate:"min=1,max=3000"`
-	UserSocials UserSocials `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user_socials"`
+	UserSocials UserSocials `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
     UpdatedAt time.Time `gorm:"autoCreateTime:nano;autoUpdateTime:nano" json:"updated_at,omitempty"`
     CreatedAt time.Time `gorm:"autoCreateTime:nano;" json:"created_at,omitempty"`
     DeletedAt *time.Time `json:"deleted_at,omitempty"`
